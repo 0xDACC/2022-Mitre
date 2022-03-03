a5462fb76086b703cfc7ff3772dde0d92649f0a6
platypushunter <dillondriskill2@gmail.COM>
2022-03-02 22:41:22 -0500

    Fixed flashing in update

--- bootloader/src/bootloader.c
+++ bootloader/src/bootloader.c
@@ -250,7 +250,7 @@ void load_firmware(uint32_t interface, uint32_t size){
         // clear flash page
         flash_erase_page(dst);
         // write flash page
-        flash_write((uint32_t )&firmware_buffer[pos], dst, FLASH_PAGE_SIZE >> 2);
+        flash_write((uint32_t *)&firmware_buffer[pos], dst, FLASH_PAGE_SIZE >> 2);
         // next page and decrease size
         dst += FLASH_PAGE_SIZE;
         remaining -= frame_size;
